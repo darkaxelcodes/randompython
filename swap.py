b74a9fb5caf853ef078ae9e370f1b09a63bc1c53
Satyam Srivastava <srivastavasatyam94@gmail.com>
2021-01-18 00:31:42 +0530

    Swapping two numbers without third variable.

---
+++ swap.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+a = a+b
+b = a-b
+a = a-b
+print("The first Number is : {}".format(a))
+print("The second Number is : {}".format(b))
